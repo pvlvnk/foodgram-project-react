5fea858f61e2df20a19fb42804fbadcfc708c74b
pvlvnk <pavlov.n98@yandex.ru>
2022-04-15 00:48:09 +0300

    added subscribing

--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -1,3 +1,4 @@
+from recipes.models import Recipe
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
@@ -13,18 +14,68 @@ class UserSerializer(ModelSerializer):
                   'last_name', 'is_subscribed',)
 
     def get_is_subscribed(self, obj):
+        print(obj)
         request = self.context.get('request')
         if request is None or request.user.is_anonymous:
             return False
         return Follow.objects.filter(user=request.user, author=obj).exists()
 
 
-# class FollowSerializer(ModelSerializer):
-#     following = serializers.SlugRelatedField(
-#         slug_field='username',
-#         queryset=User.objects.all()
-#     )
+class FollowSerializer(serializers.ModelSerializer):
+    author = UserSerializer
+    user = UserSerializer
 
-#     class Meta:
-#         model = Follow
-#         fields = ('following',)
+    class Meta:
+        model = Follow
+        fields = ('author', 'user')
+
+    def validate(self, data):
+        author = data.get('author')
+        user = data.get('user')
+        if user == author:
+            raise serializers.ValidationError(
+                {'errors': 'Нельзя подписаться на самого себя'}
+            )
+        elif Follow.objects.filter(author=author, user=user).exists():
+            raise serializers.ValidationError(
+                {'errors': 'Вы уже подписаны на данного пользователя'}
+            )
+
+    def create(self, validated_data):
+        author = validated_data.get('author')
+        user = validated_data.get('user')
+        return Follow.objects.create(user=user, author=author)
+
+
+class RecipesForSubscribeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = ('id', 'name', 'image', 'cooking_time')
+
+
+class ResponeSubscribeSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+    recipes = serializers.SerializerMethodField()
+    recipes_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ('email', 'id', 'username', 'first_name',
+                  'last_name', 'is_subscribed', 'recipes', 'recipes_count')
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+        if request is None or request.user.is_anonymous:
+            return False
+        return Follow.objects.filter(user=request.user, author=obj).exists()
+
+    def get_recipes(self, obj):
+        request = self.context.get('request')
+        recipes_limit = request.POST.get('recipes_limit')
+        queryset = Recipe.objects.filter(author=obj)
+        if recipes_limit:
+            queryset = queryset[:(recipes_limit)]
+        return RecipesForSubscribeSerializer(queryset, many=True).data
+
+    def get_recipes_count(self, obj):
+        return Recipe.objects.filter(author=obj).count()
--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -1,9 +1,58 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
+from djoser.views import UserViewSet
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from users.models import Follow, User
-# from users.serializers import FollowSerializer
+from users.serializers import FollowSerializer, ResponeSubscribeSerializer
 
+
+class CustomUserViewSet(UserViewSet):
+    pagination_class = PageNumberPagination
+
+    @action(
+        detail=True,
+        methods=['POST', 'DELETE'],
+        url_path='subscribe',
+        url_name='subscribe',
+        permission_classes=(IsAuthenticated,),
+    )
+    def subscribe(self, request, id):
+        user = request.user
+        author = get_object_or_404(User, id=id)
+        serializer = FollowSerializer(
+            data={'user': user.id, 'author': author.id}
+        )
+        if request.method == 'POST':
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            serializer = ResponeSubscribeSerializer(
+                author, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        follow = get_object_or_404(Follow, user=user, author=author)
+        follow.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(
+        detail=False,
+        methods=['GET'],
+        url_path='subscriptions',
+        url_name='subscriptions',
+        permission_classes=(IsAuthenticated,),
+    )
+    def subscriptions(self, request):
+        user = request.user
+        queryset = User.objects.filter(following__user=user)
+        pages = self.paginate_queryset(queryset)
+        serializer = ResponeSubscribeSerializer(
+            pages,
+            many=True,
+            context={'request': request}
+        )
+        return self.get_paginated_response(serializer.data)
 
 # class FollowViewSet(viewsets.ModelViewSet):
 #     serializer_class = FollowSerializer
